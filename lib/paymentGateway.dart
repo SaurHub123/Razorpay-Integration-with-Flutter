61bc437c9456cdcd5b542ab277e7eb0bc05919ad
Saurabh kumar <Saurabhkumar26121999@gmail.com>
2025-02-27 00:21:20 +0530

    final commit

---
+++ lib/paymentGateway.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class RazorpayPaymentScreen extends StatefulWidget {
+  @override
+  _RazorpayPaymentScreenState createState() => _RazorpayPaymentScreenState();
+}
+
+class _RazorpayPaymentScreenState extends State<RazorpayPaymentScreen> {
+  late Razorpay _razorpay;
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    _razorpay.clear(); 
+    super.dispose();
+  }
+
+  void openCheckout() {
+    var options = {
+      'key': '', // Replace with your Razorpay Key ID
+      'amount': 32900, 
+      'name': 'SELECT-SPORTS',
+      'description': 'Payment for Slot #893a7b48-0705-4662-989a-b0119af9b482',
+      'prefill': {'contact': '+917261854534', 'email': 'Saurabh@user.com'},
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint('Error: $e');
+    }
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Payment Successful: ${response.paymentId}")),
+    );
+
+    print("Payment Successfull");
+    print(response.data);
+    print(response.orderId);
+    print(response.paymentId);
+    print(response.signature);
+
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Payment Failed: ${response.message}")),
+    );
+    print("Payment Got Error");
+    print(response);
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          content: Text("External Wallet Selected: ${response.walletName}")),
+    );
+        print("Payment in External wallet");
+    print(response);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Razorpay Payment')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: openCheckout,
+          child: Text('Pay Now'),
+        ),
+      ),
+    );
+  }
+}
